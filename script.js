b399eb8ca5c107b2730c57f774ed7dae46843a1f
marin0suke <110573250+marin0suke@users.noreply.github.com>
2024-10-03 13:16:54 +1000

    Add getRandomColor function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256); // 255 values
+    let g = Math.floor(Math.random() * 256); 
+    let b = Math.floor(Math.random() * 256); 
+    return `rgb(${r}, ${g}, ${b})`; // 
+}
+
+
 function createGrid(size) {
     const container = document.querySelector(".container"); // grab the element we want to mutate.
     container.innerHTML = "";
@@ -9,7 +17,7 @@ function createGrid(size) {
         div.classList.add("pixel");
         console.log(div);
         div.addEventListener("mouseover", function() {
-            div.style.backgroundColor = "#333"; // colour changes on mouseover
+            div.style.backgroundColor = getRandomColor(); // colour changes on mouseover
         })
 
         container.appendChild(div);
