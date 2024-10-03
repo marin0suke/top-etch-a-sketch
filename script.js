eea452ae90bd046d79780ce22f953d1470947d72
marin0suke <110573250+marin0suke@users.noreply.github.com>
2024-10-03 13:42:57 +1000

    Add darkening filter functionality

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-
 function getRandomColor() {
-    let r = Math.floor(Math.random() * 256); // 255 values
-    let g = Math.floor(Math.random() * 256); 
-    let b = Math.floor(Math.random() * 256); 
-    return `rgb(${r}, ${g}, ${b})`; // 
+  let r = Math.floor(Math.random() * 256); // 255 values
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`; //
 }
 
-
 function createGrid(size) {
-    const container = document.querySelector(".container"); // grab the element we want to mutate.
-    container.innerHTML = "";
-    const allDivs = size * size;
+  const container = document.querySelector(".container"); // grab the element we want to mutate.
+  container.innerHTML = "";
+  const allDivs = size * size;
 
-    for (let i = 0; i < allDivs; i++) {
-        const div = document.createElement("div");
-        div.classList.add("pixel");
-        console.log(div);
-        div.addEventListener("mouseover", function() {
-            div.style.backgroundColor = getRandomColor(); // colour changes on mouseover
-        })
+  for (let i = 0; i < allDivs; i++) {
+    const div = document.createElement("div");
+    div.classList.add("pixel");
+    div.dataset.hoverCount = 0;
 
-        container.appendChild(div);
-    }
+    div.addEventListener("mouseover", function () {
+      let hoverCount = parseInt(div.dataset.hoverCount) || 0;
+
+      hoverCount++; // increment immediately after first mouseover.
+      div.dataset.hoverCount = hoverCount; //update the var after increment.
+
+      if (hoverCount === 1) {
+        div.style.backgroundColor = getRandomColor(); // colour changes on mouseover
+      } else if (hoverCount < 10) {
+        let brightness = 100 - hoverCount * 10; // 100 is brightest. hovercount increases, deduction from brightness increases until pixel is black.
+        div.style.filter = `brightness(${brightness}%)`; // HSL scale can adjust for brightness with %. Hue/saturation/lightness.
+      }
+    });
+
+    container.appendChild(div);
+  }
 }
 
 createGrid(100);
